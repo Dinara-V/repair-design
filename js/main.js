94e95b7a8ff239d7d47394a789e22606dd4204a1
Dinara-V <dinara404vagapova@gmail.com>
2020-03-16 21:59:02 +0300

    feat: added progects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/* document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close')
@@ -9,4 +9,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     element.addEventListener('click', switchModal);
   });
   closeBtn.addEventListener('click', switchModal);
+}); */
+
+/* готов ли документ к тому, чтобы мы начали с ним манипуляцию? дальше используеи безымянную функцию, которая начнёт выполняться после того, как весь документ прогрузится*/
+$(document).ready(function () {
+  /* создаём переменную modal и помещаем туда наше модальное окно*/
+  var modal = $('.modal'),
+      modalBtn =$('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+  modalBtn.on('click', function(){
+    /* по клику присваиваем модальному окну класс modal--visible*/
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
 });
